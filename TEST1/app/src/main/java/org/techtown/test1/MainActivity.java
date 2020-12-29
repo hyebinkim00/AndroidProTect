2a7cfd9ef499c0403061dc24d3410b6d06765ee8
hyebinkim00 <hyebinkim00>
2020-12-29 11:59:45 +0900

    안드로이드 연습

---
+++ TEST1/app/src/main/java/org/techtown/test1/MainActivity.java
@@ -0,0 +1,19 @@
+package org.techtown.test1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+
+}
