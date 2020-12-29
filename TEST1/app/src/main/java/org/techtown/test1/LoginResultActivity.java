5cd17ddd153663bba5e0e38285a7e6c4cd1bbd9a
hyebinkim00 <hyebinkim00>
2020-12-29 17:49:16 +0900

    정보넘기기까지

---
+++ TEST1/app/src/main/java/org/techtown/test1/LoginResultActivity.java
@@ -0,0 +1,34 @@
+package org.techtown.test1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class LoginResultActivity extends AppCompatActivity {
+
+        TextView TextView_get;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView_get = findViewById(R.id.TextView_get);
+
+        Intent intent = getIntent();
+
+        Bundle bundle = intent.getExtras();
+
+            String email = bundle.getString("email");
+            String pwd= bundle.getString("pwd");
+
+         TextView_get.setText(email+"/"+pwd);
+
+    }
+}
--- TEST1/app/src/main/java/org/techtown/test1/MainActivity.java
+++ TEST1/app/src/main/java/org/techtown/test1/MainActivity.java
@@ -2,18 +2,44 @@ package org.techtown.test1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class MainActivity extends AppCompatActivity {
+
+    TextInputEditText  TextInput_pwd,TextInput_email;
+    LinearLayout Test_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextInput_email = findViewById(R.id.TextInput_email);
+        TextInput_pwd = findViewById(R.id.TextInput_pwd);
+        Test_login = findViewById(R.id.Test_login);
+
+        String email = TextInput_email.getText().toString();
+        String pwd = TextInput_pwd.getText().toString();
+
+        Test_login.setClickable(true);
+        Test_login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String email = TextInput_email.getText().toString();
+                String pwd = TextInput_pwd.getText().toString();
+
+                Intent intent= new Intent(MainActivity.this,LoginResultActivity.class);
+                intent.putExtra("email",email);
+                intent.putExtra("pwd",pwd);
+                startActivity(intent);
+
+            }
+        });
     }
-
-
-
 }
